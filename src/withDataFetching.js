d59563201aab4bb02dce0bffe91837b2e0f0f2c6
Irena Jurasek <jurasek@eurisnet.it>
2021-03-15 10:57:01 +0100

    Refactor withDataFetching

--- src/withDataFetching.js
+++ src/withDataFetching.js
@@ -1,41 +1,43 @@
-import React from 'react'
+import React from "react";
 
 export default function withDataFetching(WrappedComponent) {
-    return class extends React.Component {
-        state = {
-            data: [],
-            loading: true,
-            error: ''
-        }
-        async componentDidMount() {
-            try {
-                const data = await fetch(this.props.dataSource)
-                const dataJSON = await data.json();
+  class WithDataFetching extends React.Component {
+    state = {
+      data: [],
+      loading: true,
+      error: "",
+    };
+    async componentDidMount() {
+      try {
+        const data = await fetch(this.props.dataSource);
+        const dataJSON = await data.json();
 
-                if(dataJSON) {
-                    this.setState({
-                        data: dataJSON,
-                        loading: false
-                    })
-                }
-            } catch(error) {
-                this.setState({
-                    loading: false,
-                    error: error.message
-                })
-            }
+        if (dataJSON) {
+          this.setState({
+            data: dataJSON,
+            loading: false,
+          });
         }
-        render(){
-            const {data, loading, error} = this.state;
-
-            return (
-                <WrappedComponent
-                    data={data}
-                    loading={loading}
-                    error={error}
-                    {...this.props}
-                    />
-            )
-        }
+      } catch (error) {
+        this.setState({
+          loading: false,
+          error: error.message,
+        });
+      }
     }
+    render() {
+      const { data, loading, error } = this.state;
+
+      return (
+        <WrappedComponent
+          data={data}
+          loading={loading}
+          error={error}
+          {...this.props}
+        />
+      );
+    }
+  }
+  WithDataFetching.displayName = `WithDataFetching(${WrappedComponent.name})`;
+  return WithDataFetching;
 }
