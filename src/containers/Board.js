255c9a8285ad4de80d959477262568d4b8dc59fe
Irena Jurasek <jurasek@eurisnet.it>
2021-03-15 10:30:31 +0100

    add withDataFetcing HOC

--- src/containers/Board.js
+++ src/containers/Board.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
-import Lane from '../components/Lane/Lane';
+import React, { Component } from "react";
+import styled from "styled-components";
+import withDataFetching from "../withDataFetching";
+import Lane from "../components/Lane/Lane";
 
 const BoardWrapper = styled.div`
   display: flex;
@@ -13,59 +14,75 @@ const BoardWrapper = styled.div`
   }
 `;
 
-class Board extends Component {
-  state = {
-    data: [],
-    loading: true,
-    error: ''
-  }
+const Board = ({ lanes, loading, error, data }) => (
+  <BoardWrapper>
+    {lanes.map((lane) => (
+      <Lane
+        key={lane.id}
+        title={lane.title}
+        loading={loading}
+        error={error}
+        tickets={data.filter((ticket) => ticket.lane === lane.id)}
+      />
+    ))}
+  </BoardWrapper>
+);
 
-  async componentDidMount() {
-    try {
-      const tickets = await fetch('../../assets/data.json');
-      //console.log(tickets)
-      const ticketsJSON = await tickets.json();
-      console.log((ticketsJSON))
+export default withDataFetching(Board);
 
-      if(ticketsJSON) {
-        this.setState({
-          data: ticketsJSON,
-          loading: false
-        })
-      }
-    } catch (error) {
-      this.setState({
-        loading: false,
-        error: error.message
-      })
-    }
-  }
+// class Board extends Component {
+//   state = {
+//     data: [],
+//     loading: true,
+//     error: ''
+//   }
 
-  render() {
-    const {data, loading, error} = this.state;
-    const lanes = [
-      { id: 1, title: 'To Do' },
-      { id: 2, title: 'In Progress' },
-      { id: 3, title: 'Review' },
-      { id: 4, title: 'Done' },
-    ];
+//   async componentDidMount() {
+//     try {
+//       const tickets = await fetch('../../assets/data.json');
+//       //console.log(tickets)
+//       const ticketsJSON = await tickets.json();
+//       console.log((ticketsJSON))
 
-    return (
-      <BoardWrapper>
-        {/* map through lanes array of objects with 4 ids */}
-        {/* filter data where each item's lane matches one of lane ids */}
-        {lanes.map(lane => (
-          <Lane 
-          key={lane.id} 
-          title={lane.title}
-          loading={loading}
-          error={error}
-          tickets={data.filter(ticket => ticket.lane === lane.id)}
-           />
-        ))}
-      </BoardWrapper>
-    );
-  }
-}
+//       if(ticketsJSON) {
+//         this.setState({
+//           data: ticketsJSON,
+//           loading: false
+//         })
+//       }
+//     } catch (error) {
+//       this.setState({
+//         loading: false,
+//         error: error.message
+//       })
+//     }
+//   }
 
-export default Board;
+//   render() {
+//     const {data, loading, error} = this.state;
+//     const lanes = [
+//       { id: 1, title: 'To Do' },
+//       { id: 2, title: 'In Progress' },
+//       { id: 3, title: 'Review' },
+//       { id: 4, title: 'Done' },
+//     ];
+
+//     return (
+//       <BoardWrapper>
+//         {/* map through lanes array of objects with 4 ids */}
+//         {/* filter data where each item's lane matches one of lane ids */}
+//         {lanes.map(lane => (
+//           <Lane
+//           key={lane.id}
+//           title={lane.title}
+//           loading={loading}
+//           error={error}
+//           tickets={data.filter(ticket => ticket.lane === lane.id)}
+//            />
+//         ))}
+//       </BoardWrapper>
+//     );
+//   }
+// }
+
+// export default Board;
